04058acd8aa02b11b331ce76c8a1849df03e9bc5
CrazyProger1 <crazyproger1@gmail.com>
2022-07-20 20:44:37 +0300

    Add: base cpp file structure

---
+++ cpp_code/base.cpp
@@ -0,0 +1,21 @@
+{% block includes %}
+
+{% endblock %}
+
+{% block builtin_types %}
+
+{% endblock %}
+
+
+{% block builtin_functions %}
+
+{% endblock %}
+
+
+{{ external_code }}
+
+
+
+int main(){
+    {{ internal_code }}
+}
