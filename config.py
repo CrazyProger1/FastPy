ff1f4e408d4b306e2b6f80675918bdacd4e5f0a6
CrazyProger1 <crazyproger1@gmail.com>
2022-07-21 10:27:10 +0300

    Add: arguments parsing

--- config.py
+++ config.py
@@ -4,4 +4,7 @@ import os
 CONFIG_FOLDER = 'config'
 
 # argparse config
-ARGPARSE_CONFIG = fastpy.JsonConfig(os.path.join(CONFIG_FOLDER, 'argparse.json'))
+ARGPARSE_CONFIG = fastpy.JsonConfig(
+    filepath=os.path.join(CONFIG_FOLDER, 'argparse.json'),
+    authoload=True
+)
--- main.py
+++ main.py
@@ -1,5 +1,22 @@
 import fastpy
 import argparse
+from config import *
+
+
+def setup_argparse() -> argparse.ArgumentParser:
+    argparser = argparse.ArgumentParser(**ARGPARSE_CONFIG['parser'])
+
+    for argument_config in ARGPARSE_CONFIG['arguments']:
+        argparser.add_argument(*argument_config['args'], **argument_config['kwargs'])
+
+    return argparser
+
+
+def main():
+    argparser = setup_argparse()
+    parsed_args = argparser.parse_args()
+    print(parsed_args)
+
 
 if __name__ == '__main__':
-    pass
+    main()
