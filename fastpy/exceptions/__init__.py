ddedc1c2fb31be3f91a52c2a72e4dcd03d15d9ba
CrazyProger1 <crazyproger1@gmail.com>
2022-07-21 10:33:24 +0300

    Add: step errors

--- fastpy/exceptions/__init__.py
+++ fastpy/exceptions/__init__.py
@@ -6,3 +6,15 @@ class CompilationError(BaseError):
     pass
 
 
+class LexingError(BaseError):
+    pass
+
+
+class ParsingError(BaseError):
+    pass
+
+
+class TranspilingError(BaseError):
+    pass
+
+
