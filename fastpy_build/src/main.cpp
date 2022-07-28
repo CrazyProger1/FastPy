eaa755882730ddea243de0090a59f655d5f06ac7
CrazyProger1 <crazyproger1@gmail.com>
2022-07-28 19:33:43 +0300

    Fix: decorator & directive symbols

--- fastpy_build/src/main.cpp
+++ fastpy_build/src/main.cpp
@@ -20,7 +20,20 @@ int main(){
 int age = input<int>("Input your age $");
 
 
+if (age > 17) {
 log_info(name, true);
+
+}
+
+if (age > 15 && age < 18) {
+log_warning(name, true);
+
+}
+
+if (age < 16) {
+log_error(name, true);
+
+}
 
 
 }
